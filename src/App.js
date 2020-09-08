69a930f6360cbe704a358da8ad5fed8caddf5297
JonathanPaita <jonathan.austria.paita@gmail.com>
2020-09-08 21:53:44 +0800

    commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { CardList } from "./components/card-list/card-list.component";
+import "./App.css";
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(
+      (monsters = monsters.name
+        .toLowerCase()
+        .inlcludes(searchField.toLowerCase()))
+    );
+
+    return (
+      <div className="App">
+        <input
+          type="search"
+          placeholder="search monsters"
+          onChange={(e) => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
+}
+
+export default App;
